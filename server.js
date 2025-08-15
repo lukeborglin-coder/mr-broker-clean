69860fc48c9724c8f5c15d68b02c63010af9fd3c
lukeborglin-coder <lukewilliamborglin@gmail.com>
2025-08-15 12:32:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -53,10 +53,15 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 // prevent stale UI while iterating
 app.use((req, res, next) => {
-  if (/\.(html|css|js)$/i.test(req.path)) res.set("Cache-Control", "no-store");
+  if (/.\.(html|css|js)$/i.test(req.path)) {
+  res.set("Cache-Control","no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.set("Pragma","no-cache");
+  res.set("Expires","0");
+}
   next();
 });
 
